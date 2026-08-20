e3647e5344c6679b7a8af0acd76cbf9a54f1eea5
Adam <41438776+Abecid@users.noreply.github.com>
2026-08-20 01:12:13 -0700

    Export finite-transition RL primitives

--- fastvideo/train/methods/rl/common/__init__.py
+++ fastvideo/train/methods/rl/common/__init__.py
@@ -1,6 +1,20 @@
 # SPDX-License-Identifier: Apache-2.0
 """Reusable RL training primitives."""
 
+from fastvideo.train.methods.rl.common.finite_transition import (
+    append_data_endpoint,
+    clipped_grpo_loss,
+    effective_sample_size,
+    endpoint_anchor_parameters,
+    gaussian_log_prob_mean,
+    group_advantages,
+    mean_pairwise_rms,
+    posterior_projection_loss,
+    reward_tilted_weights,
+    sample_diagonal_gaussian,
+    temporal_l1,
+    validate_training_schedule,
+)
 from fastvideo.train.methods.rl.common.sampling import (
     DiffusionSampler,
     SamplingConfig,
@@ -23,8 +37,20 @@ __all__ = [
     "RLValidationConfig",
     "SamplingConfig",
     "SamplingResult",
+    "append_data_endpoint",
+    "clipped_grpo_loss",
     "distributed_k_repeat_indices",
+    "effective_sample_size",
+    "endpoint_anchor_parameters",
+    "gaussian_log_prob_mean",
+    "group_advantages",
+    "mean_pairwise_rms",
     "media_to_video_array",
+    "posterior_projection_loss",
+    "reward_tilted_weights",
+    "sample_diagonal_gaussian",
+    "temporal_l1",
+    "validate_training_schedule",
     "validation_caption",
     "validation_shard_indices",
 ]
