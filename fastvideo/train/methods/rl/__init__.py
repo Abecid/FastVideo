5a5db0805d57dd1b48d3a49464c7b54f769ccd20
Adam <41438776+Abecid@users.noreply.github.com>
2026-08-22 00:46:06 -0700

    [misc] export calibrated audited finite-transition methods

--- fastvideo/train/methods/rl/__init__.py
+++ fastvideo/train/methods/rl/__init__.py
@@ -11,8 +11,16 @@ from fastvideo.train.methods.rl.finite_transition_posterior_repro import (
 from fastvideo.train.methods.rl.finite_transition_reliable import (
     ReliableFiniteTransitionMethod,
 )
+from fastvideo.train.methods.rl.finite_transition_reliable_audited import (
+    AuditedReliableFiniteTransitionMethod,
+)
+from fastvideo.train.methods.rl.finite_transition_reliable_calibrated import (
+    CalibratedReliableFiniteTransitionMethod,
+)
 
 __all__ = [
+    "AuditedReliableFiniteTransitionMethod",
+    "CalibratedReliableFiniteTransitionMethod",
     "DiffusionNFTMethod",
     "FiniteTransitionPosteriorMethod",
     "ReliableFiniteTransitionMethod",
