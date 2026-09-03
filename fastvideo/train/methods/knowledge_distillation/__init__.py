750b7e1ea62a8626e9e9322b029411a84776b54c
Adam <41438776+Abecid@users.noreply.github.com>
2026-09-02 18:20:51 -0700

    feat: export H3 PeRFlow training method

--- fastvideo/train/methods/knowledge_distillation/__init__.py
+++ fastvideo/train/methods/knowledge_distillation/__init__.py
@@ -1,9 +1,17 @@
 # SPDX-License-Identifier: Apache-2.0
 
+from fastvideo.train.methods.knowledge_distillation.h3_perflow import (
+    H3PeRFlowMethod,
+)
 from fastvideo.train.methods.knowledge_distillation.h3_rest import H3RESTMethod
 from fastvideo.train.methods.knowledge_distillation.kd import (
     KDCausalMethod,
     KDMethod,
 )
 
-__all__ = ["H3RESTMethod", "KDCausalMethod", "KDMethod"]
+__all__ = [
+    "H3PeRFlowMethod",
+    "H3RESTMethod",
+    "KDCausalMethod",
+    "KDMethod",
+]
